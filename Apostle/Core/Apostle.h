8986b9f9c0ff2a7a1d71251111418fe853bb6b57
canarda13 <cokcuoglu15@ku.edu.tr>
2023-04-22 17:06:25 +0300

    Inputdefs

---
+++ Apostle/Core/Apostle.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#ifndef APOSTLE
+#define APOSTLE
+#include "ApostleCore.h"
+
+#ifdef VULKAN_RENDERING_CONTEXT
+#include <vulkan/vulkan.h>
+#endif
+
+#ifdef GLFW_WINDOWING_SYSTEM 
+#include <GLFW/glfw3.h>
+#endif
+#endif
---
+++ Apostle/Core/ApostleCore.h
@@ -0,0 +1,12 @@
+#pragma once
+
+
+#ifndef APOSTLE_CORE
+#define APOSTLE_CORE
+
+//defining rendering context type, i.e Render Engine (OpenGL, Vulkan, etc.)
+#define VULKAN_RENDERING_CONTEXT
+
+//defining the windowing system
+#define GLFW_WINDOWING_SYSTEM
+#endif
